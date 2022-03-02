4585d1cb83a9379a0559042c6abec37d24b889dd
Andrew Owen <yumaikas94@gmail.com>
2022-03-02 10:15:22 -0700

    Clean up deploy .zip file

--- webPrep.ps1
+++ webPrep.ps1
@@ -1,16 +1,32 @@
+param([switch] $serve)
 try {
-$paths = ls -Exclude projects\,drafts\  | ? { -not(($_.FullName -ilike "*love-bins*") -or ($_.FullName -ilike "*.ps1") -or ($_.FullName -ilike "*publish*")) } | % { $_.FullName; }
 
+$paths = ls | ? { -not(($_.FullName -ilike "*love-bins*") `
+                    -or ($_.FullName -ilike "*.ps1") `
+                    -or ($_.FullName -ilike "*publish*") `
+                    -or ($_.FullName -ilike "*\projects") `
+                    -or ($_.FullName -ilike "*\.git\*") `
+                    ) } | % { $_.FullName; }
+
+echo $paths
 
 ls -Recurse .\publish\faultLines\ | Remove-Item -Recurse -Force 
 
 # New-Item .\publish\faultLines\ -ItemType Directory
 Remove-Item -Recurse -Force .\publish\faultLines.love
 Compress-Archive $paths publish\faultLines.love
+
 Push-location publish\
-"Fault Lines" | npx love.js faultLInes.love faultLines -c -m (16777216*2)
+"Fault Lines" | npx love.js faultLines.love faultLines -c -m (16777216*2)
+
+Copy-Item ..\projects\html\index.html .\faultLines\index.html
+
 cd faultLines
+Compress-Archive (ls) ..\faultLines.zip
+
+if ($serve) {
 web-dir
+}
 }
 finally {
     Pop-location
